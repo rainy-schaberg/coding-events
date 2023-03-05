8bb08375ddffe2f05901c613751a53660dcdd050
Rainy <112306653+rainy-moss@users.noreply.github.com>
2023-03-05 10:51:01 -0600

    Added all files from work done in coding-events-demo

---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,73 @@
+package org.launchcode.codingevents.models;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+public class Event {
+
+    private int id;
+    private static int nextId = 1;
+    @NotBlank
+    @Size (min = 3, max = 50, message = "Name must be within 3-50 characters.")
+    private String name;
+    @Size(max = 500, message="Description too long.")
+    private String description;
+    @NotBlank
+    @Email(message = "Invalid email, try again!")
+    private String contactEmail;
+
+
+    public Event(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public int getId() {
+        return id;
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event)) return false;
+        Event event = (Event) o;
+        return getId() == event.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
